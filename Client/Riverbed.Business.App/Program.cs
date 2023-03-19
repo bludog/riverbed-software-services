303f80a610766b229423f515c8c1755f266055f3
James Bond <shadowdog007@gmail.com>
2023-03-18 20:40:37 -0500

    Add the TelerikRootComponent to the MainLayout.razor to complete the Telerik Setup.

--- Client/Riverbed.Business.App/Program.cs
+++ Client/Riverbed.Business.App/Program.cs
@@ -38,6 +38,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseCors("AllowAll"); app.UseExceptionHandler("/Error");
     app.UseMigrationsEndPoint();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 else
 {

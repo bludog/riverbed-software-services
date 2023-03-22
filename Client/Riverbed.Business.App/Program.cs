c6e104a5085f71b9c87c695acfbbcde20db0cb95
James Bond <shadowdog007@gmail.com>
2023-03-21 21:40:24 -0500

    Working and deployed to WinHost IIS.

--- Client/Riverbed.Business.App/Program.cs
+++ Client/Riverbed.Business.App/Program.cs
@@ -36,7 +36,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors("AllowAll"); app.UseExceptionHandler("/Error");
+    app.UseCors("AllowAll");
     app.UseMigrationsEndPoint();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
